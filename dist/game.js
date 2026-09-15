70a1100a1e6ceb370b2c2ef436c68412edb242ba
jalapenoseed <94670345+jalapenoseed@users.noreply.github.com>
2026-09-14 20:19:25 -0500

    remove stub game.js so quiet-start file can be uploaded

--- dist/game.js
+++
@@ -1,3 +0,0 @@
-// Temporary loader: this connector cannot upload the 140KB game.js body.
-// Loads the last intact copy from commit e3e85c2 on this repo.
-import 'https://cdn.jsdelivr.net/gh/jalapenoseed/GRIDRUNNER@e3e85c2e8e7cc9cc2c0bf774343aa997fa160267/dist/game.js';
